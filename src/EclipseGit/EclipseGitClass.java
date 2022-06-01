331b4bb1589e4cafba47ddf88b412a178078cd77
NightKnight313 <gmazurki@gmail.com>
2022-06-01 19:13:23 -0400

    1st commit

---
+++ src/EclipseGit/EclipseGitClass.java
@@ -0,0 +1,10 @@
+package EclipseGit;
+
+public class EclipseGitClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("hi its me again ");
+	}
+
+}
